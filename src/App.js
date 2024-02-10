b6ccb9ac25d0ec68130e1c42c1873f56cb4549cd
morteza-hoseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2024-02-10 11:14:12 +0330

    FEAT: remove extra files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useRoutes } from "react-router-dom"
-import routes from "./components/routes";
+import Navbar from "./components/navbar/navbar";
+import { Box } from "@mui/material";
+// import { useRoutes } from "react-router-dom";
+// import routes from "./components/routes";
 
 export default function App() {
-
-  let Route = useRoutes(routes)
+  // let Route = useRoutes(routes);
 
   return (
-    <>
-    </>
+    <html dir="ltr" lang="en">
+      <Box>
+        <Navbar />
+      </Box>
+    </html>
   );
 }
-
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <Container>
+      <Grid container>
+        <Typography variant="h5">Morteza</Typography>
+      </Grid>
+    </Container>
+  );
+}
